a5222c4ba1095f1a724a41167d3c00445898afba
BJH9 <tfzangin9@gmail.com>
2023-03-20 18:49:53 +0900

    init

---
+++ src/main/java/com/csee/hanspace/HanspaceApplication.java
@@ -0,0 +1,13 @@
+package com.csee.hanspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csee/hanspace/HanspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csee.hanspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
