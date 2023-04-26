3b1b04747413b40d8e6c3888e9033359a617fa5a
BJH9 <tfzangin9@gmail.com>
2023-04-27 01:07:02 +0900

    reserve
    Signed-off-by: BJH9 <tfzangin9@gmail.com>

--- src/main/java/com/csee/hanspace/application/service/ReserveService.java
+++ src/main/java/com/csee/hanspace/application/service/ReserveService.java
@@ -1,14 +1,8 @@
 package com.csee.hanspace.application.service;
 
-import com.csee.hanspace.domain.entity.ReserveRecord;
-import com.csee.hanspace.application.dto.MyReserveDto;
 import com.csee.hanspace.domain.repository.ReserveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
