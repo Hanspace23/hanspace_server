b09847f5c9fe2ac01bd3782bd09cd999fa7c1a4b
lukehongg <lukehongg@handong.ac.kr>
2023-04-28 18:48:27 +0900

    Room DTO

---
+++ src/main/java/com/csee/hanspace/application/dto/RoomFilterDto.java
@@ -0,0 +1,40 @@
+package com.csee.hanspace.application.dto;
+
+import com.csee.hanspace.domain.entity.Room;
+import com.csee.hanspace.domain.entity.RoomTags;
+import com.csee.hanspace.domain.entity.TimeRecord;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomFilterDto {
+
+    private Long id;
+    private String roomName;
+    private List<RoomTags> tags;
+    private int availableStart;
+    private int availableEnd;
+    private String img;
+    private String detail;
+    private List<TimeRecord> reservedList;
+
+    public static RoomFilterDto from(Room room){
+        return RoomFilterDto.builder()
+                .id(room.getId())
+                .roomName(room.getName())
+                .tags(room.getRoomTagsList())
+                .availableStart(room.getStartTime())
+                .availableEnd(room.getEndTime())
+                .img(room.getImage())
+                .detail(room.getDescription())
+                .reservedList(room.getTimeRecordList())
+                .build();
+    }
+
+
+}
--- src/main/java/com/csee/hanspace/application/service/RoomService.java
+++ src/main/java/com/csee/hanspace/application/service/RoomService.java
@@ -1,17 +1,28 @@
 package com.csee.hanspace.application.service;
 
+import com.csee.hanspace.application.dto.RoomFilterDto;
 import com.csee.hanspace.domain.entity.Room;
 import com.csee.hanspace.domain.repository.ReserveRepository;
 import com.csee.hanspace.domain.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RoomService {
+    @Autowired
     private final RoomRepository roomRepository;
+
+    @Transactional(readOnly = true)
+    public List<RoomFilterDto> findAllRoomBySiteId(Long sid) {
+        List<Room> ret = roomRepository.findAllRoomBySiteId(sid);
+        return ret.stream().map(RoomFilterDto::from).collect(Collectors.toList());
+    }
 
 
 //    public List<Room> findAllRoomForCalendar(Long sid) {
